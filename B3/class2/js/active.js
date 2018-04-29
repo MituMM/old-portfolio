1789e400061cfa3f0aa414198e6e42c06628fb80
MariamMitu <mitu.mariam2018@gmail.com>
2018-04-30 00:33:57 +0600

    file added

---
+++ B3/class2/js/active.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').owlCarousel({
+        items:1,
+       nav:true,
+         loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: ['<i class="fa fa-caret-left" ></i>','<i class="fa fa-caret-right" ></i>']
+       
+      
+       
+
+    });
+});
